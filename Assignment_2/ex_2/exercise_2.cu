1812e268fb79bc69a737a2bd586a1888f29b4df4
Pontus Asp <pontus.asp@gmail.com>
2020-11-05 14:30:09 +0100

    changed host arrays to use malloc instead of stack memory

--- Assignment_2/ex_2/exercise_2.cu
+++ Assignment_2/ex_2/exercise_2.cu
@@ -2,7 +2,7 @@
 #include <random>
 #include <cmath>
 
-#define ARRAY_SIZE 100000
+#define ARRAY_SIZE 10000
 #define BLOCK_SIZE 256
 
 __global__ void device_saxpy(float* x, float* y, const float a)
@@ -26,9 +26,8 @@ int main()
 
     const float a = 1.0;
 
-    float x[ARRAY_SIZE];
-    float y[ARRAY_SIZE];
-    float d_res[ARRAY_SIZE];
+    float* x = (float*)malloc(ARRAY_SIZE * sizeof(float));
+    float* y = (float*)malloc(ARRAY_SIZE * sizeof(float));
 
     for (int i = 0; i < ARRAY_SIZE; i++) {
         x[i] = dist(rdmGen);
@@ -42,35 +41,33 @@ int main()
     cudaMalloc(&d_x, ARRAY_SIZE * sizeof(float));
     cudaMalloc(&d_y, ARRAY_SIZE * sizeof(float));
 
-    cudaMemcpy(d_x, &x, ARRAY_SIZE * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_y, &y, ARRAY_SIZE * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_x, x, ARRAY_SIZE * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, y, ARRAY_SIZE * sizeof(float), cudaMemcpyHostToDevice);
 
-    printf("Computing SAXPY on the CPU... ");
+
     // call host func
+    printf("Computing SAXPY on the CPU... ");
     host_saxpy(x, y, a);
     printf("Done!\n\n");
 
 
-    printf("Computing SAXPY on the GPU... ");
     // call device func
+    printf("Computing SAXPY on the GPU... ");
     device_saxpy<<<(ARRAY_SIZE + BLOCK_SIZE - 1) / BLOCK_SIZE,
         BLOCK_SIZE>>>(d_x, d_y, a);
-
-    // Wait for device to finish
     cudaDeviceSynchronize();
     printf("Done!\n\n");
 
-    // Get results from device and store in d_res
-    cudaMemcpy(&d_res, d_y, ARRAY_SIZE * sizeof(float), cudaMemcpyDeviceToHost);
 
-    printf("Comparing the output for each implementation... ");
+    // Get results from device and store in d_res
+    cudaMemcpy(x, d_y, ARRAY_SIZE * sizeof(float), cudaMemcpyDeviceToHost);
 
     // compare d_res to y
+    printf("Comparing the output for each implementation... ");
 
     bool correct = true;
-
     for(int i = 0; i < ARRAY_SIZE; i++) {
-        if(abs(d_res[i] - y[i]) > 0.0001) {
+        if(abs(x[i] - y[i]) > 0.0001) { // x is device result, y is host result
             correct = false;
             break;
         }
@@ -78,6 +75,10 @@ int main()
 
     if(correct) printf("Correct!\n");
     else printf("Incorrect!\n");
+
+    // Free up resources
+    free(y);
+    free(x);
 
     cudaFree(d_y);
     cudaFree(d_x);
