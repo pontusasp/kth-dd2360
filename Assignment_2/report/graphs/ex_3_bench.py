2b9b2527bfc05059651c5053cf1171b385955db3
Pontus Asp <pontus.asp@gmail.com>
2020-11-09 16:27:11 +0100

    finished graphs for a2-ex-3 and wrote report until ex-3 part 3

--- Assignment_2/report/graphs/ex_3_bench.py
+++ Assignment_2/report/graphs/ex_3_bench.py
@@ -3,7 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import re
 
-particleSizes = ["10000", "100000", "1000000", "10000000"]
+particleSizes = ["10k", "100k", "1000k", "10000k"]
 blockSizes = ["16", "32", "64", "128", "256"]
 
 
@@ -13,6 +13,7 @@ f = open('ex_3_gpu_output.dat')
 raw = f.readlines()
 f.close()
 
+cntr = 0
 for t in range(0, len(particleSizes)):
        deviceList = []
 
@@ -20,17 +21,22 @@ for t in range(0, len(particleSizes)):
               deviceRun = 0
               hostRun = 0
               for i in range(0, 5):
-                     deviceRun = deviceRun + float(raw[t*len(particleSizes) + x*5 + i])
+                     deviceRun = deviceRun + float(raw[cntr])
+                     print(cntr)
+                     cntr = cntr + 1
               deviceList.append(deviceRun / 5)
 
        fig, ax = plt.subplots()
 
+       ylevel = max(0, 2 * min(deviceList) - max(deviceList))
+
        ax.set(xlabel='Block Size', ylabel='Average time (s) (5 samples)',
-              title=' CUDA GPU Simulation: 1k Iterations, ' + str(int(int(particleSizes[t])/1000)) + 'k Particles')
+              title=' CUDA GPU Simulation: 1k Iterations, ' + particleSizes[t] + ' Particles')
        ax.grid()
        ax.bar(blockSizes, deviceList)
+       ax.set_ylim(bottom=ylevel)
 
-       fig.savefig("ex_3_graph_gpu_" + str(int(int(particleSizes[t])/1000)) + "k.png")
+       fig.savefig("ex_3_graph_gpu_" + particleSizes[t] + ".png")
 
 
 
@@ -48,14 +54,14 @@ for t in range(0, len(particleSizes)):
        hostRun = 0
        for i in range(0, 5):
               hostRun = hostRun + float(raw[t * len(particleSizes) + i])
-       hostList.append(hostRun / 5)
+       hostList.append(str(round(hostRun / 5, 3)))
 
 fig, ax = plt.subplots()
 
 ax.set(xlabel='Particle count', ylabel='Average time (s) (5 samples)',
-       title='CPU Simulation: 1k Iterations, ' + str(int(int(particleSizes[t])/1000)) + 'k Particles')
+       title='CPU Simulation: 1k Iterations')
 ax.grid()
 ax.bar(particleSizes, hostList)
 
-fig.savefig("ex_3_graph_cpu_" + str(int(int(particleSizes[t])/1000)) + "k.png")
+fig.savefig("ex_3_graph_cpu.png")
 
