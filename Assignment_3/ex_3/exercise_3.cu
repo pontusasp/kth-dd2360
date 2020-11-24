ebe8c96bb12217bcc206417780b95312d7ffc755
Pontus Asp <pontus.asp@gmail.com>
2020-11-25 00:50:30 +0100

    finished ex_3

--- Assignment_3/ex_3/exercise_3.cu
+++ Assignment_3/ex_3/exercise_3.cu
@@ -2,6 +2,8 @@
 #include <random>
 #include <chrono>
 
+#define BATCH_SIZE 20000
+
 typedef struct
 {
     float3 pos = {0.0};
@@ -79,13 +81,13 @@ int main(int argc, char **argv)
     std::uniform_real_distribution<float> velDist(-10.0, 10.0);
 
     for(int i = 0; i < num_particles; i++) {
-        particles[i].pos.x = posDist(rdmGen);
-        particles[i].pos.y = posDist(rdmGen);
-        particles[i].pos.z = posDist(rdmGen);
+        d_res[i].pos.x = particles[i].pos.x = posDist(rdmGen);
+        d_res[i].pos.y = particles[i].pos.y = posDist(rdmGen);
+        d_res[i].pos.z = particles[i].pos.z = posDist(rdmGen);
 
-        particles[i].vel.x = velDist(rdmGen);
-        particles[i].vel.y = velDist(rdmGen);
-        particles[i].vel.z = velDist(rdmGen);
+        d_res[i].vel.x = particles[i].vel.x = velDist(rdmGen);
+        d_res[i].vel.y = particles[i].vel.y = velDist(rdmGen);
+        d_res[i].vel.z = particles[i].vel.z = velDist(rdmGen);
     }
 
     float4 forces = {
@@ -95,6 +97,49 @@ int main(int argc, char **argv)
          1.0    // dt
     };
 
+
+    /*
+
+    === Example ===
+    ...
+    int N = 3;
+    int *arr, *d_arr;
+    cudaMallocHost(&arr, N * sizeof(int));
+    cudaMalloc(&d_arr, N * sizeof(int));
+
+    cudaStream_t s_id;
+    cudaStreamCreate(&s_id);
+
+    cudaMemcpyAsync(d_arr, arr, N * sizeof(int), cudaMemcpyHostToDevice, s_id);
+
+    // 3rd parameter is shared device memory
+    fun<<<block_size, blocks, 0, s_id>>>;
+
+    cudaStreamSynchronize(s_id);
+    cudaMemcpyAsync(arr, d_arr, N * sizeof(int), cudaMemcpyDeviceToHost, s_id);
+
+    cudaStreamDestroy(s_id);
+    ...
+    === = = = = ===
+
+
+    === Lecture ===
+    ...
+    for (int i = 0; i < nStreams; i++) {
+
+        int offset = i * streamSize;
+        
+        cudaMemcpyAsync(&d_a[offset], &a[offset], streamBytes, cudaMemcpyHostToDevice, stream[i]);
+
+        kernel<<<streamSize/blockSize, blockSize, 0, stream[i]>>>(d_a, offset);
+
+        cudaMemcpyAsync(&a[offset], &d_a[offset], streamBytes, cudaMemcpyDeviceToHost, stream[i]);
+
+    }
+    ...
+    === = = = = ===
+
+    */
 
 
     // ============= START COMPUTING ON DEVICE ============== //
@@ -106,16 +151,44 @@ int main(int argc, char **argv)
         
         // Create, allocate and copy array to device
         Particle* d_particles = 0;
-        cudaMalloc(&d_particles, num_particles * sizeof(Particle));
+
+        int num_streams = (num_particles + BATCH_SIZE - 1) / BATCH_SIZE;
+
+        // Allocate memory for num_particles + potential padding
+        cudaMalloc(&d_particles, num_streams * BATCH_SIZE * sizeof(Particle));
+
+        cudaStream_t *s_id = (cudaStream_t*)malloc(num_streams * sizeof(cudaStream_t));
+
+        for (int i = 0; i < num_streams; i++) {
+            cudaStreamCreate(&s_id[i]);
+        }
 
         for(int i = 0; i < num_iterations; i++) {
-            cudaMemcpy(d_particles, particles, num_particles * sizeof(Particle), cudaMemcpyHostToDevice);
+            
+            for(int j = 0; j < num_streams; j++) {
+                int offset = j * BATCH_SIZE;
 
-            device_timestep<<<(num_particles + block_size - 1) / block_size,
-                block_size>>>(d_particles, forces);
-                
-            cudaDeviceSynchronize();
-            cudaMemcpy(d_res, d_particles, num_particles * sizeof(Particle), cudaMemcpyDeviceToHost);
+                cudaMemcpyAsync(&d_particles[offset], &d_res[offset],
+                    BATCH_SIZE * sizeof(Particle), cudaMemcpyHostToDevice, s_id[j]);
+
+                device_timestep<<<(BATCH_SIZE + block_size - 1) / block_size,
+                    block_size, 0, s_id[j]>>>(d_particles, forces);
+                    
+                cudaMemcpyAsync(&d_res[offset], &d_particles[offset],
+                    num_particles * sizeof(Particle), cudaMemcpyDeviceToHost, s_id[j]);
+            }
+
+            // Basically like cudaDeviceSynchronize, but using this to memorize stream synchronization
+            for(int j = 0; j < num_streams; j++) {
+                cudaStreamSynchronize(s_id[j]);
+            }
+
+            // Ready to do stuff on host...
+            
+        }
+
+        for (int i = 0; i < num_streams; i++) {
+            cudaStreamDestroy(s_id[i]);
         }
 
         cudaFree(d_particles);
