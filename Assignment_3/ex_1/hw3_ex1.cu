e812f09e087824f60126eb91343c692b1bbaca02
Pontus Asp <pontus.asp@gmail.com>
2020-11-25 01:18:26 +0100

    uncommented cpu code

--- Assignment_3/ex_1/hw3_ex1.cu
+++ Assignment_3/ex_1/hw3_ex1.cu
@@ -428,7 +428,7 @@ int main(int argc, char **argv)
     {
         // Launch the CPU version
         gettimeofday(&t[0], NULL);
-        //cpu_grayscale(bitmap.width, bitmap.height, bitmap.data, image_out[0]);
+        cpu_grayscale(bitmap.width, bitmap.height, bitmap.data, image_out[0]);
         gettimeofday(&t[1], NULL);
         
         elapsed[0] = get_elapsed(t[0], t[1]);
@@ -458,7 +458,7 @@ int main(int argc, char **argv)
     {
         // Launch the CPU version
         gettimeofday(&t[0], NULL);
-        //cpu_gaussian(bitmap.width, bitmap.height, image_out[0], image_out[1]);
+        cpu_gaussian(bitmap.width, bitmap.height, image_out[0], image_out[1]);
         gettimeofday(&t[1], NULL);
         
         elapsed[0] = get_elapsed(t[0], t[1]);
@@ -482,7 +482,7 @@ int main(int argc, char **argv)
     {
         // Launch the CPU version
         gettimeofday(&t[0], NULL);
-        //cpu_sobel(bitmap.width, bitmap.height, image_out[1], image_out[0]);
+        cpu_sobel(bitmap.width, bitmap.height, image_out[1], image_out[0]);
         gettimeofday(&t[1], NULL);
         
         elapsed[0] = get_elapsed(t[0], t[1]);
